9dec334c4c9f3cfc688197911c80bf3c0d43270b
Nicole Bartholomew <nabartholomew@gmail.com>
2019-01-23 13:23:04 -0600

    (feat) Completed loopingATriangle exercise.

---
+++ chapter_02/loopingATriangle.js
@@ -0,0 +1,16 @@
+
+let block = "#";
+
+for (let count = 1; count <= 7; count++){
+	let line = block.repeat(count);
+	console.log(line);
+}
+
+
+/* Book Answer
+
+for (let line = "#"; line.length < 8; line += "#")
+  console.log(line);
+
+  */
+ 
