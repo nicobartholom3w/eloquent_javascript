f48f356586d1967f92016f7d71bfb28f71c49c44
Nicole Bartholomew <nabartholomew@gmail.com>
2019-03-13 14:54:18 -0500

    (feat) Initial solution for balloon.

---
+++ chapter_15/balloon.js
@@ -0,0 +1,23 @@
+ document.addEventListener("DOMContentLoaded", function(event){
+
+ 	// window.addEventListener("keydown", event => {
+ 	// 	let currentFontSize;
+ 	// 	let newFontSize;
+ 	// 	if(event.key == "ArrowUp") {
+ 	// 		document.body.style.fontSize = "2rem";
+ 	// 	}
+ 	// });
+ 	// window.addEventListener("keydown", event => {
+ 	// 	if(event.key == "ArrowDown") {
+ 	// 		// currentFontSize = document.body.style.fontSize;
+ 	// 		// newFontSize = currentFontSize - currentFontSize * 0.10;
+ 	// 		// document.body.style.fontSize = newFontSize;
+ 	// 		document.body.style.fontSize = -10%;
+ 	// 	}
+ 	// })
+
+
+
+
+
+});
