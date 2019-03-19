56421e6bef3eb84c9df1867f47731a03a3b53d0c
Nicole Bartholomew <nabartholomew@gmail.com>
2019-03-19 15:24:28 -0500

    (feat) Completed balloon and added/started mouseTrail.js exercise.

--- chapter_15/balloon.js
+++ chapter_15/balloon.js
@@ -1,22 +1,30 @@
  document.addEventListener("DOMContentLoaded", function(event){
 
- 	// window.addEventListener("keydown", event => {
- 	// 	let currentFontSize;
- 	// 	let newFontSize;
- 	// 	if(event.key == "ArrowUp") {
- 	// 		document.body.style.fontSize = "2rem";
- 	// 	}
- 	// });
- 	// window.addEventListener("keydown", event => {
- 	// 	if(event.key == "ArrowDown") {
- 	// 		// currentFontSize = document.body.style.fontSize;
- 	// 		// newFontSize = currentFontSize - currentFontSize * 0.10;
- 	// 		// document.body.style.fontSize = newFontSize;
- 	// 		document.body.style.fontSize = -10%;
- 	// 	}
- 	// })
-
-
+	window.addEventListener("keydown", event => {
+ 		let currentFontSize;
+ 		let newFontSize;
+ 		if(event.key == "ArrowUp") {
+ 			let body = document.querySelector("body");
+ 			let style = window.getComputedStyle(body, null).getPropertyValue('font-size');
+ 			currentFontSize = parseFloat(style);
+				newFontSize = currentFontSize + currentFontSize * 0.10;
+				if (newFontSize > 240){
+					document.body.innerHTML = document.body.innerHTML.replace("🎈", "💥" );
+				}
+ 			document.body.style.fontSize = newFontSize + "px";
+ 		}
+	});
+	window.addEventListener("keydown", event => {
+		let currentFontSize;
+ 		let newFontSize;
+ 		if(event.key == "ArrowDown") {
+ 			let body = document.querySelector("body");
+ 			let style = window.getComputedStyle(body, null).getPropertyValue('font-size');
+ 			currentFontSize = parseFloat(style);
+			newFontSize = currentFontSize - currentFontSize * 0.10;
+ 			document.body.style.fontSize = newFontSize + "px";
+ 		}
+ 	});
 
 
 
---
+++ chapter_15/mouseTrail.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(event){
+
+	
+	window.addEventListener("mousemove", event => {
+
+		let dots = [];
+		let mouse = {
+			x: 0,
+			y: 0
+		};
+
+		
+
+
+	})
+
+});
