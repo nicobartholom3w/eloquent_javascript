46bcdc21142e065b6cef1e374e6f51eaca0e808c
Nicole Bartholomew <nabartholomew@gmail.com>
2019-03-21 15:49:09 -0500

    (feat) First attempt at adding divs to an array to repeat pattern of mousetrail.

--- chapter_15/mouseTrail.js
+++ chapter_15/mouseTrail.js
@@ -5,11 +5,19 @@ document.addEventListener("DOMContentLoaded", function(event){
 
 		let dots = [];
 
-		let n = document.createElement("div");
-		n.className = "trail";
-		document.body.appendChild(n);
-		n.style.left = (event.pageX) + "px";
-		n.style.top = (event.pageY) + "px";
+
+		for(let i = 0; i < 10; i++){
+			let n = document.createElement("div");
+			n.className = "trail";
+			document.body.appendChild(n);
+			n.style.left = (event.pageX) + "px";
+			n.style.top = (event.pageY) + "px";
+			dots.push(n);
+		}
+
+		for (let j = 10; i > 0; i--){
+			dots.pop(n);
+		}
 		
 
 
