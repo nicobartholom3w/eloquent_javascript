6488461e74cafe8f130b5c223a52411ea284bf9b
Nicole Bartholomew <nabartholomew@gmail.com>
2019-03-22 09:48:03 -0500

    (feat) Currently have solution so that one div is following mouse around. Still working on finding full solution.

--- chapter_15/mouseTrail.js
+++ chapter_15/mouseTrail.js
@@ -1,26 +1,29 @@
 document.addEventListener("DOMContentLoaded", function(event){
 
+	let dots = [];
+	let n;
+
+	for (let i = 0; i < 5; i++){
+		n = document.createElement("div");
+		n.className = "trail";
+		dots.push(n);
+	}
 	
-	window.addEventListener("mousemove", event => {
-
-		let dots = [];
-
-
-		for(let i = 0; i < 10; i++){
-			let n = document.createElement("div");
-			n.className = "trail";
-			document.body.appendChild(n);
-			n.style.left = (event.pageX) + "px";
-			n.style.top = (event.pageY) + "px";
-			dots.push(n);
-		}
-
-		for (let j = 10; i > 0; i--){
-			dots.pop(n);
-		}
+	window.addEventListener("mousemove", event => {	
+	// 		document.body.appendChild(dots[count]);
+	// 		dots[count].style.left = (event.pageX) + "px";
+	// 		dots[count].style.top = (event.pageY) + "px";
+	// 		count++;
+	// 		dots.push(dots.shift(dots[count]));
+	// })
+	let count = 0;
 		
-
-
+		while (count < 5) {
+			document.body.appendChild(dots[count]);
+			dots[count].style.left = (event.pageX) + "px";
+			dots[count].style.top = (event.pageY) + "px";
+			count++;
+		}
+			dots.push(dots.shift(dots[count]));
 	})
-
 });
