23761ba4150526934e2f063b796152051132c679
Nicole Bartholomew <nabartholomew@gmail.com>
2019-03-23 20:13:56 -0500

    (feat) Initial commit for tabs.js. Progress on mouseTrail.js exercise.

--- chapter_15/mouseTrail.js
+++ chapter_15/mouseTrail.js
@@ -8,22 +8,19 @@ document.addEventListener("DOMContentLoaded", function(event){
 		n.className = "trail";
 		dots.push(n);
 	}
-	
-	window.addEventListener("mousemove", event => {	
-	// 		document.body.appendChild(dots[count]);
-	// 		dots[count].style.left = (event.pageX) + "px";
-	// 		dots[count].style.top = (event.pageY) + "px";
-	// 		count++;
-	// 		dots.push(dots.shift(dots[count]));
-	// })
 	let count = 0;
-		
-		while (count < 5) {
-			document.body.appendChild(dots[count]);
-			dots[count].style.left = (event.pageX) + "px";
-			dots[count].style.top = (event.pageY) + "px";
-			count++;
+	window.addEventListener("mousemove", event => {	
+		document.body.appendChild(dots[count]);
+		dots[count].style.left = (event.pageX) + "px";
+		dots[count].style.top = (event.pageY) + "px";
+		count++;
+		if (count > 5){
+			// let r = dots.shift(dots[0]);
+			// dots.push(r);
+			dots[0].style.left = (event.pageX) + "px";
+			dots[0].style.top = (event.pageY) + "px";
 		}
-			dots.push(dots.shift(dots[count]));
 	})
+
+
 });
---
+++ chapter_15/tabs.js
@@ -0,0 +1,7 @@
+ document.addEventListener("DOMContentLoaded", function(event){
+
+   function asTabs(node) {
+      // Your code here.
+    }
+    asTabs(document.querySelector("tab-panel"));
+  });
