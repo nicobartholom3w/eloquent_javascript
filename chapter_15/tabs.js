875ec3348f5fd1092195f352ee046bbc0a583497
Nicole Bartholomew <nabartholomew@gmail.com>
2019-04-02 13:01:26 -0500

    (feat) Completed initial solution to tabs.js exercise.

--- chapter_15/tabs.js
+++ chapter_15/tabs.js
@@ -4,33 +4,56 @@
  	let buttonArray = [];
  	let divsArray = document.querySelectorAll("[data-tabname]");
  	let buttonName;
-	let count;	
+ 	let button;
+
 
    function asTabs(node) {
-      // Your code here.
-
 	  	for(let div of divsArray){
 	  		buttonName = div.getAttribute("data-tabname");
-	  		let button = document.createElement("button");
+	  		button = document.createElement("button");
 	  		button.innerHTML = buttonName;
 	  		buttonArray.push(button);
+		  	tabPanel[0].insertBefore(button, divsArray[0]);
+	  	}	
 	  	
-	  		// if (buttonName == "one"){
-		  		tabPanel[0].insertBefore(button, divsArray[0]);
-		  		count = 0;
-	  		// }
-	  		// else {
-	  		// 	tabPanel[0].insertBefore(button, );
-	  		// 	count++;
-	  		// }
-	  	}
-		
+    	for (let tab of buttonArray){
+    		tab.style.cursor = "pointer";
+    	}
     }
     	
     function selectTab(node) {
+    	divsArray[1].style.display = "none";
+    	divsArray[2].style.display = "none";
+    	buttonArray[1].style.opacity = "0.25";
+    	buttonArray[2].style.opacity = "0.25";
 
+    	buttonArray[0].addEventListener("click", () => {
+    		divsArray[0].style.display = "block";
+    		divsArray[1].style.display = "none";
+	    	divsArray[2].style.display = "none";
+	    	buttonArray[0].style.opacity = "1";
+	    	buttonArray[1].style.opacity = "0.25";
+	    	buttonArray[2].style.opacity = "0.25";
+    	});
+    	buttonArray[1].addEventListener("click", () => {
+    		divsArray[0].style.display = "none";
+    		divsArray[1].style.display = "block";
+	    	divsArray[2].style.display = "none";
+	    	buttonArray[0].style.opacity = "0.25";
+	    	buttonArray[1].style.opacity = "1";
+	    	buttonArray[2].style.opacity = "0.25";
+    	});
+    	buttonArray[2].addEventListener("click", () => {
+    		divsArray[0].style.display = "none";
+    		divsArray[1].style.display = "none";
+	    	divsArray[2].style.display = "block";
+	    	buttonArray[0].style.opacity = "0.25";
+	    	buttonArray[1].style.opacity = "0.25";
+	    	buttonArray[2].style.opacity = "1";
+    	});
+    }
 
-    }	
+	
 
 
     asTabs(tabPanel[0]);
