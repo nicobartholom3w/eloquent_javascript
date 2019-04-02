02a83445336d6bafce73f4d5fb3e2ca8ee27ec7f
Nicole Bartholomew <nabartholomew@gmail.com>
2019-04-02 12:07:32 -0500

    (feat) Completed the function for creating the top buttons/tabs.

--- chapter_15/tabs.js
+++ chapter_15/tabs.js
@@ -1,7 +1,40 @@
  document.addEventListener("DOMContentLoaded", function(event){
 
+ 	let tabPanel = document.getElementsByClassName("tab-panel");
+ 	let buttonArray = [];
+ 	let divsArray = document.querySelectorAll("[data-tabname]");
+ 	let buttonName;
+	let count;	
+
    function asTabs(node) {
       // Your code here.
+
+	  	for(let div of divsArray){
+	  		buttonName = div.getAttribute("data-tabname");
+	  		let button = document.createElement("button");
+	  		button.innerHTML = buttonName;
+	  		buttonArray.push(button);
+	  	
+	  		// if (buttonName == "one"){
+		  		tabPanel[0].insertBefore(button, divsArray[0]);
+		  		count = 0;
+	  		// }
+	  		// else {
+	  		// 	tabPanel[0].insertBefore(button, );
+	  		// 	count++;
+	  		// }
+	  	}
+		
     }
-    asTabs(document.querySelector("tab-panel"));
+    	
+    function selectTab(node) {
+
+
+    }	
+
+
+    asTabs(tabPanel[0]);
+    selectTab(tabPanel[0]);
+
+
 });
